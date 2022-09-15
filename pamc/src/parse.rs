8f202e044ba4ff463ad90e940f6b47c621941ccd
Kyle Lin <kylejlin.us@gmail.com>
2022-09-14 17:58:59 -0700

    Keep working on parser

--- pamc/src/parse.rs
+++ pamc/src/parse.rs
@@ -14,20 +14,24 @@ pub fn parse_file(tokens: Vec<Token>) -> Result<File, ParseError> {
     } else {
         return Ok(File(vec![]));
     };
-    let mut stack: Vec<StackItem> = vec![StackItem::File(Box::new(StackFile {
-        first_token,
-        items: vec![],
-    }))];
+    let mut stack: Vec<UnfinishedStackItem> =
+        vec![UnfinishedStackItem::File(Box::new(UnfinishedFile {
+            first_token,
+            items: vec![],
+        }))];
 
     for token in tokens.into_iter().filter(is_not_whitespace) {
-        stack.push(StackItem::Token(token));
-        while stack.len() >= 2 {
-            let popped = stack.pop().unwrap();
-            let top = stack.last_mut().unwrap();
-            let accept_result = top.accept(popped);
+        let mut finished = FinishedStackItem::Token(token);
+        while stack.len() >= 1 {
+            let top_unfinished = stack.last_mut().unwrap();
+            let accept_result = top_unfinished.accept(finished);
             match accept_result {
                 AcceptResult::ContinueToNextToken => break,
-                AcceptResult::ContinueReducing => continue,
+                AcceptResult::PopAndContinueReducing(new_finished) => {
+                    stack.pop();
+                    finished = new_finished;
+                    continue;
+                }
                 AcceptResult::Push(item) => {
                     stack.push(item);
                     break;
@@ -48,87 +52,147 @@ fn is_not_whitespace_ref(token: &&Token) -> bool {
     token.kind != TokenKind::Whitespace
 }
 
-#[derive(Clone, Debug)]
-enum StackItem {
-    File(Box<StackFile>),
-    Token(Token),
-    Type(StackTypeStatement),
-    Let(StackLetStatement),
+use unfinished::*;
+mod unfinished {
+    use super::*;
+
+    #[derive(Clone, Debug)]
+    pub enum UnfinishedStackItem {
+        File(Box<UnfinishedFile>),
+        Type(UnfinishedTypeStatement),
+        Let(UnfinishedLetStatement),
+    }
+
+    #[derive(Clone, Debug)]
+    pub struct UnfinishedFile {
+        pub first_token: Token,
+        pub items: Vec<FileItem>,
+    }
+
+    #[derive(Clone, Debug)]
+    pub enum UnfinishedTypeStatement {
+        Keyword(Token),
+        Name(Token, Identifier),
+        Params(Token, Identifier, Vec<Param>),
+        Constructors(Token, Identifier, Vec<Constructor>),
+    }
+
+    #[derive(Clone, Debug)]
+    pub enum UnfinishedLetStatement {
+        Keyword(Token),
+        Name(Token, Identifier),
+        NameEqual(Token, Identifier),
+        ValueEqual(Token, Identifier, Expression),
+    }
 }
 
-#[derive(Clone, Debug)]
-struct StackFile {
-    first_token: Token,
-    items: Vec<FileItem>,
+mod first_token {
+    use super::*;
+
+    impl FinishedStackItem {
+        pub fn first_token(&self) -> &Token {
+            match self {
+                FinishedStackItem::Token(token) => &token,
+                FinishedStackItem::Type(token, _) => &token,
+                FinishedStackItem::Let(token, _) => &token,
+            }
+        }
+    }
 }
 
-#[derive(Clone, Debug)]
-enum StackTypeStatement {
-    Keyword(Token),
-    Name(Token, Identifier),
-    Params(Token, Identifier, Vec<Param>),
-    Constructors(Token, Identifier, Vec<Constructor>),
+use finished::*;
+mod finished {
+    use super::*;
+
+    #[derive(Clone, Debug)]
+    pub enum FinishedStackItem {
+        Token(Token),
+        Type(
+            /// First token
+            Token,
+            TypeStatement,
+        ),
+        Let(
+            /// First token
+            Token,
+            LetStatement,
+        ),
+    }
 }
 
-#[derive(Clone, Debug)]
-enum StackLetStatement {
-    Keyword(Token),
-    Name(Token, Identifier),
-}
-
-#[derive(Clone, Debug)]
-enum AcceptResult {
-    ContinueToNextToken,
-    ContinueReducing,
-    Push(StackItem),
-    Error(ParseError),
-}
-
+use accept::*;
 mod accept {
     use super::*;
 
-    impl StackItem {
-        pub fn accept(&mut self, item: StackItem) -> AcceptResult {
+    pub trait Accept {
+        fn accept(&mut self, item: FinishedStackItem) -> AcceptResult;
+    }
+
+    #[derive(Clone, Debug)]
+    pub enum AcceptResult {
+        ContinueToNextToken,
+        PopAndContinueReducing(FinishedStackItem),
+        Push(UnfinishedStackItem),
+        Error(ParseError),
+    }
+
+    impl Accept for UnfinishedStackItem {
+        fn accept(&mut self, item: FinishedStackItem) -> AcceptResult {
             match self {
-                StackItem::File(file) => file.accept(item),
-                StackItem::Token(token) => {
-                    AcceptResult::Error(ParseError::UnexpectedToken(token.clone()))
-                }
+                UnfinishedStackItem::File(file) => file.accept(item),
+                UnfinishedStackItem::Type(type_) => type_.accept(item),
+                UnfinishedStackItem::Let(let_) => let_.accept(item),
             }
         }
     }
-    impl StackFile {
-        fn accept(&mut self, item: StackItem) -> AcceptResult {
+
+    impl Accept for UnfinishedFile {
+        fn accept(&mut self, item: FinishedStackItem) -> AcceptResult {
             match item {
-                StackItem::File(file) => {
-                    AcceptResult::Error(ParseError::UnexpectedToken(file.first_token))
-                }
-                StackItem::Token(token) => match token.kind {
-                    TokenKind::TypeLowerCase => {
-                        AcceptResult::Push(StackItem::Type(StackTypeStatement::Keyword(token)))
-                    }
-                    TokenKind::Let => {
-                        AcceptResult::Push(StackItem::Let(StackLetStatement::Keyword(token)))
-                    }
+                FinishedStackItem::Token(token) => match token.kind {
+                    TokenKind::TypeLowerCase => AcceptResult::Push(UnfinishedStackItem::Type(
+                        UnfinishedTypeStatement::Keyword(token),
+                    )),
+                    TokenKind::Let => AcceptResult::Push(UnfinishedStackItem::Let(
+                        UnfinishedLetStatement::Keyword(token),
+                    )),
                     _ => AcceptResult::Error(ParseError::UnexpectedToken(token)),
                 },
-                StackItem::Type(type_) => {
-                    self.items.push(type_.into());
-                    AcceptResult::ContinueReducing
+                FinishedStackItem::Type(_, type_) => {
+                    self.items.push(FileItem::Type(type_));
+                    AcceptResult::ContinueToNextToken
+                }
+                FinishedStackItem::Let(_, let_) => {
+                    self.items.push(FileItem::Let(let_));
+                    AcceptResult::ContinueToNextToken
                 }
             }
         }
     }
-}
 
-mod into_unbound_ast_node {
-    use super::*;
-
-    impl From<StackTypeStatement> for FileItem {
-        fn from(let_: StackTypeStatement) -> FileItem {
-            FileItem::Type(TypeStatement {
-                // TODO
-            })
+    impl Accept for UnfinishedTypeStatement {
+        fn accept(&mut self, item: FinishedStackItem) -> AcceptResult {
+            match self {
+                UnfinishedTypeStatement::Keyword(type_kw) => match item {
+                    FinishedStackItem::Token(token) => match token.kind {
+                        TokenKind::Identifier => {
+                            let name = Identifier {
+                                start_index: token.start_index,
+                                content: token.content.clone(),
+                            };
+                            *self = UnfinishedTypeStatement::Name(type_kw.clone(), name);
+                            AcceptResult::ContinueToNextToken
+                        }
+                    },
+                    other => AcceptResult::Error(ParseError::UnexpectedToken(
+                        other.first_token().clone(),
+                    )),
+                },
+            }
         }
+    }
+
+    impl Accept for UnfinishedLetStatement {
+        fn accept(&mut self, item: FinishedStackItem) -> AcceptResult {}
     }
 }
