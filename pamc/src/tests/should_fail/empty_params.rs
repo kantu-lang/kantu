3f11622d6ab372666166dd080ec2f4a34865a329
Kyle Lin <kylejlin.us@gmail.com>
2022-09-26 12:00:05 -0700

    Deduplicate code in empty_params

--- pamc/src/tests/should_fail/empty_params.rs
+++ pamc/src/tests/should_fail/empty_params.rs
@@ -1,8 +1,6 @@
 use super::*;
 
-#[test]
-fn empty_type_params() {
-    let src = include_str!("../sample_code/should_fail/empty_parens/empty_type_params.ph");
+fn expect_rparen_parse_error(src: &str) {
     let file_id = FileId(0);
     let tokens = lex(src).expect("Lexing failed");
     let err = parse_file(tokens, file_id).expect_err("Parsing unexpectedly succeeded");
@@ -12,74 +10,40 @@ fn empty_type_params() {
         }
         _ => panic!("Unexpected error: {:#?}", err),
     }
+}
+
+#[test]
+fn empty_type_params() {
+    let src = include_str!("../sample_code/should_fail/empty_parens/empty_type_params.ph");
+    expect_rparen_parse_error(src);
 }
 
 #[test]
 fn empty_variant_params() {
     let src = include_str!("../sample_code/should_fail/empty_parens/empty_variant_params.ph");
-    let file_id = FileId(0);
-    let tokens = lex(src).expect("Lexing failed");
-    let err = parse_file(tokens, file_id).expect_err("Parsing unexpectedly succeeded");
-    match err {
-        ParseError::UnexpectedToken(token) => {
-            assert_eq!(token.kind, TokenKind::RParen);
-        }
-        _ => panic!("Unexpected error: {:#?}", err),
-    }
+    expect_rparen_parse_error(src);
 }
 
 #[test]
 fn empty_fun_params() {
     let src = include_str!("../sample_code/should_fail/empty_parens/empty_fun_params.ph");
-    let file_id = FileId(0);
-    let tokens = lex(src).expect("Lexing failed");
-    let err = parse_file(tokens, file_id).expect_err("Parsing unexpectedly succeeded");
-    match err {
-        ParseError::UnexpectedToken(token) => {
-            assert_eq!(token.kind, TokenKind::RParen);
-        }
-        _ => panic!("Unexpected error: {:#?}", err),
-    }
+    expect_rparen_parse_error(src);
 }
 
 #[test]
 fn empty_call_params() {
     let src = include_str!("../sample_code/should_fail/empty_parens/empty_call_params.ph");
-    let file_id = FileId(0);
-    let tokens = lex(src).expect("Lexing failed");
-    let err = parse_file(tokens, file_id).expect_err("Parsing unexpectedly succeeded");
-    match err {
-        ParseError::UnexpectedToken(token) => {
-            assert_eq!(token.kind, TokenKind::RParen);
-        }
-        _ => panic!("Unexpected error: {:#?}", err),
-    }
+    expect_rparen_parse_error(src);
 }
 
 #[test]
 fn empty_forall_params() {
     let src = include_str!("../sample_code/should_fail/empty_parens/empty_forall_params.ph");
-    let file_id = FileId(0);
-    let tokens = lex(src).expect("Lexing failed");
-    let err = parse_file(tokens, file_id).expect_err("Parsing unexpectedly succeeded");
-    match err {
-        ParseError::UnexpectedToken(token) => {
-            assert_eq!(token.kind, TokenKind::RParen);
-        }
-        _ => panic!("Unexpected error: {:#?}", err),
-    }
+    expect_rparen_parse_error(src);
 }
 
 #[test]
 fn empty_match_case_params() {
     let src = include_str!("../sample_code/should_fail/empty_parens/empty_match_case_params.ph");
-    let file_id = FileId(0);
-    let tokens = lex(src).expect("Lexing failed");
-    let err = parse_file(tokens, file_id).expect_err("Parsing unexpectedly succeeded");
-    match err {
-        ParseError::UnexpectedToken(token) => {
-            assert_eq!(token.kind, TokenKind::RParen);
-        }
-        _ => panic!("Unexpected error: {:#?}", err),
-    }
+    expect_rparen_parse_error(src);
 }
