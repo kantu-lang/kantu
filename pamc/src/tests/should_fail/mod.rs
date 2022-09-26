e5347767d472f72b9c45155e75858a6321a19729
Kyle Lin <kylejlin.us@gmail.com>
2022-09-26 16:10:24 -0700

    Remove unused import

--- pamc/src/tests/should_fail/mod.rs
+++ pamc/src/tests/should_fail/mod.rs
@@ -7,9 +7,7 @@ use crate::{
     data::{node_registry::NodeRegistry, symbol_provider::SymbolProvider, FileId},
     processing::{
         bind_type_independent::{bind_symbols_to_identifiers, BindError},
-        extract_variant_type_args::{
-            extract_variant_type_args_for_file, IllegalVariantReturnTypeError,
-        },
+        extract_variant_type_args::extract_variant_type_args_for_file,
         lex::lex,
         parse::parse_file,
         register::register_file,
