6798d13372e0bfa81cc19be8820f8aace497a089
Kyle Lin <kylejlin.us@gmail.com>
2022-09-27 14:26:38 -0700

    Id-ify extract_variant_type_args.rs

--- pamc/src/processing/extract_variant_type_args.rs
+++ pamc/src/processing/extract_variant_type_args.rs
@@ -1,5 +1,7 @@
 use crate::data::{
-    node_registry::NodeId, registered_ast::*, symbol_database::SymbolDatabase,
+    node_registry::{NodeId, NodeRegistry},
+    registered_ast::*,
+    symbol_database::SymbolDatabase,
     variant_return_type::VariantReturnTypeTypeArgsMap,
 };
 
@@ -8,12 +10,19 @@ pub struct IllegalVariantReturnTypeError(pub NodeId<WrappedExpression>);
 
 pub fn extract_variant_type_args_for_file(
     symbol_db: &SymbolDatabase,
+    registry: &NodeRegistry,
     file: &File,
 ) -> Result<VariantReturnTypeTypeArgsMap, IllegalVariantReturnTypeError> {
     let mut map = VariantReturnTypeTypeArgsMap::empty();
-    for item in &file.items {
+    for item_id in &file.item_ids {
+        let item = &registry.wrapped_file_item(*item_id).item;
         if let FileItem::Type(type_statement) = item {
-            extract_variant_type_args_for_type_statement(symbol_db, &mut map, type_statement)?;
+            extract_variant_type_args_for_type_statement(
+                symbol_db,
+                registry,
+                &mut map,
+                type_statement,
+            )?;
         }
     }
     Ok(map)
@@ -21,11 +30,13 @@ pub fn extract_variant_type_args_for_file(
 
 fn extract_variant_type_args_for_type_statement(
     symbol_db: &SymbolDatabase,
+    registry: &NodeRegistry,
     map: &mut VariantReturnTypeTypeArgsMap,
     type_statement: &TypeStatement,
 ) -> Result<(), IllegalVariantReturnTypeError> {
-    for variant in &type_statement.variants {
-        let args = get_variant_type_args(symbol_db, type_statement, variant)?;
+    for variant_id in &type_statement.variant_ids {
+        let variant = registry.variant(*variant_id);
+        let args = get_variant_type_args(symbol_db, registry, type_statement, variant)?;
         map.insert_new(variant.id, args);
     }
     Ok(())
@@ -33,30 +44,36 @@ fn extract_variant_type_args_for_type_statement(
 
 fn get_variant_type_args(
     symbol_db: &SymbolDatabase,
+    registry: &NodeRegistry,
     type_statement: &TypeStatement,
     variant: &Variant,
 ) -> Result<Vec<NodeId<WrappedExpression>>, IllegalVariantReturnTypeError> {
-    let type_symbol = symbol_db.identifier_symbols.get(type_statement.name.id);
-    match &variant.return_type.expression {
+    let type_symbol = symbol_db.identifier_symbols.get(type_statement.name_id);
+    let return_type_id = variant.return_type_id;
+    let return_type = &registry.wrapped_expression(return_type_id).expression;
+    match return_type {
         Expression::Identifier(identifier) => {
             let identifier_symbol = symbol_db.identifier_symbols.get(identifier.id);
             if identifier_symbol == type_symbol {
                 Ok(vec![])
             } else {
-                Err(IllegalVariantReturnTypeError(variant.return_type.id))
+                Err(IllegalVariantReturnTypeError(return_type_id))
             }
         }
-        Expression::Call(call) => match &call.callee.expression {
-            Expression::Identifier(identifier) => {
-                let identifier_symbol = symbol_db.identifier_symbols.get(identifier.id);
-                if identifier_symbol == type_symbol {
-                    Ok(call.args.iter().map(|arg| arg.id).collect())
-                } else {
-                    Err(IllegalVariantReturnTypeError(variant.return_type.id))
+        Expression::Call(call) => {
+            let callee = &registry.wrapped_expression(call.callee_id).expression;
+            match callee {
+                Expression::Identifier(identifier) => {
+                    let identifier_symbol = symbol_db.identifier_symbols.get(identifier.id);
+                    if identifier_symbol == type_symbol {
+                        Ok(call.arg_ids.clone())
+                    } else {
+                        Err(IllegalVariantReturnTypeError(return_type_id))
+                    }
                 }
+                _other_callee => Err(IllegalVariantReturnTypeError(return_type_id)),
             }
-            _other_callee => Err(IllegalVariantReturnTypeError(variant.return_type.id)),
-        },
-        _other_variant_return_type => Err(IllegalVariantReturnTypeError(variant.return_type.id)),
+        }
+        _other_variant_return_type => Err(IllegalVariantReturnTypeError(return_type_id)),
     }
 }
