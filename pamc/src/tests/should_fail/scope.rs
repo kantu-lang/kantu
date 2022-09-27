baf66d68bfc71af0e68e874d973de16ec0c026bd
Kyle Lin <kylejlin.us@gmail.com>
2022-09-26 21:54:26 -0700

    Reorder function declarations

--- pamc/src/tests/should_fail/scope.rs
+++ pamc/src/tests/should_fail/scope.rs
@@ -27,17 +27,6 @@ fn expect_invalid_dot_rhs_error(src: &str, expected_unfindable_name: &str) {
     });
 }
 
-fn expect_bind_error(src: &str, panicker: impl Fn(BindError, &NodeRegistry)) {
-    let file_id = FileId(0);
-    let tokens = lex(src).expect("Lexing failed");
-    let file = parse_file(tokens, file_id).expect("Parsing failed");
-    let mut registry = NodeRegistry::empty();
-    let file_id = register_file(&mut registry, file);
-    let err = bind_symbols_to_identifiers(&registry, vec![file_id])
-        .expect_err("Binding unexpectedly succeeded");
-    panicker(err, &registry);
-}
-
 #[test]
 fn reference_let_in_body() {
     let src = include_str!("../sample_code/should_fail/scope/ref_let_in_body.ph");
@@ -60,6 +49,17 @@ fn reference_fun_in_param() {
 fn reference_fun_in_return_type() {
     let src = include_str!("../sample_code/should_fail/scope/ref_fun_in_return_type.ph");
     expect_name_not_found_error(src, "g");
+}
+
+fn expect_bind_error(src: &str, panicker: impl Fn(BindError, &NodeRegistry)) {
+    let file_id = FileId(0);
+    let tokens = lex(src).expect("Lexing failed");
+    let file = parse_file(tokens, file_id).expect("Parsing failed");
+    let mut registry = NodeRegistry::empty();
+    let file_id = register_file(&mut registry, file);
+    let err = bind_symbols_to_identifiers(&registry, vec![file_id])
+        .expect_err("Binding unexpectedly succeeded");
+    panicker(err, &registry);
 }
 
 #[test]
