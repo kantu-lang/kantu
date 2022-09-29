83bff6cdfa12eeb5bd8d62a2f7db003d7c3ed65b
Kyle Lin <kylejlin.us@gmail.com>
2022-09-28 23:04:50 -0700

    Backup 1

--- pamc/src/processing/type_check.rs
+++ pamc/src/processing/type_check.rs
@@ -1,8 +1,9 @@
 use crate::data::{
-    node_registry::{NodeId, NodeRegistry},
+    node_registry::{ListId, NodeId, NodeRegistry},
     registered_ast::*,
     symbol_database::{Symbol, SymbolDatabase, SymbolSource},
     type_map::{NormalFormNodeId, TypeMap},
+    variant_return_type::VariantReturnTypeTypeArgsMap,
 };
 
 #[derive(Clone, Debug)]
@@ -30,6 +31,7 @@ pub enum TypeError {
 pub fn type_check_file(
     registry: &mut NodeRegistry,
     symbol_db: &mut SymbolDatabase,
+    variant_return_type_args: &VariantReturnTypeTypeArgsMap,
     file_id: NodeId<File>,
 ) -> Result<TypeMap, TypeError> {
     let file = registry.file(file_id);
@@ -37,6 +39,7 @@ pub fn type_check_file(
     let mut state = TypeCheckState {
         registry,
         symbol_db,
+        variant_return_type_args,
         context: TypeCheckContext::new(),
         type0_identifier_id: todo(),
     };
@@ -62,7 +65,7 @@ fn type_check_type_statement(
     type_statement_id: NodeId<TypeStatement>,
 ) -> Result<(), TypeError> {
     let type_statement = state.registry.type_statement(type_statement_id);
-    let param_ids: Vec<NodeId<Param>> = state
+    let param_ids = state
         .registry
         .param_list(type_statement.param_list_id)
         .to_vec();
@@ -73,26 +76,7 @@ fn type_check_type_statement(
     let type_name_type_id = if param_ids.is_empty() {
         state.type0_identifier_id
     } else {
-        let normalized_param_ids: Vec<NodeId<Param>> = param_ids
-            .iter()
-            .map(|id| -> Result<NodeId<Param>, TypeError> {
-                let param = state.registry.param(*id);
-                let param_type_id = param.type_id;
-                let normalized_param_with_dummy_id = Param {
-                    id: dummy_id(),
-                    is_dashed: param.is_dashed,
-                    name_id: param.name_id,
-                    // It's safe to call `evaluate_well_typed_expression`
-                    // because we type-checked it above.
-                    type_id: evaluate_well_typed_expression(state, param_type_id)?.0,
-                };
-                Ok(state
-                    .registry
-                    .add_param_and_overwrite_its_id(normalized_param_with_dummy_id))
-            })
-            .collect::<Result<Vec<_>, TypeError>>()?;
-        let normalized_param_list_id = state.registry.add_param_list(normalized_param_ids);
-
+        let normalized_param_list_id = normalize_params(state, param_ids.iter().copied())?;
         let forall_with_dummy_id = Forall {
             id: dummy_id(),
             param_list_id: normalized_param_list_id,
@@ -144,10 +128,25 @@ fn type_check_variant(
     variant_id: NodeId<Variant>,
 ) -> Result<(), TypeError> {
     let variant = state.registry.variant(variant_id);
-    let param_ids: Vec<NodeId<Param>> = state.registry.param_list(variant.param_list_id).to_vec();
-    for param_id in param_ids {
-        type_check_param(state, param_id)?;
+    let param_ids = state.registry.param_list(variant.param_list_id).to_vec();
+    for param_id in &param_ids {
+        type_check_param(state, *param_id)?;
     }
+
+    let normalized_param_list_id = normalize_params(state, param_ids.iter().copied())?;
+
+    let return_type_type_args = state.variant_return_type_args.get(variant_id);
+
+    let variant_type_id = if param_ids.is_empty() {
+        let call_id = state.registry.add_call_and_overwrite_its_id(Call {
+            id: dummy_id(),
+            callee_id: variant.return_type_id,
+            arg_list_id: ListId::new(),
+        });
+    } else {
+        unimplemented!();
+    };
+
     // We need to add the type for the declared variant to the context.
     // If `variant` is a variant of type T, then the type of `variant` is either
     // `T` or `forall() { T }`.
@@ -319,6 +318,37 @@ fn get_normalized_type(
     evaluate_well_typed_expression(state, unnormalized_type_id)
 }
 
+// TODO: This should just lookup the param symbol in the context,
+// since this should only be called after the param has been type checked.
+// We should probably remove this entirely.
+
+/// Every param id yielded by `param_ids` **must** be a param that has been
+/// type checked.
+fn normalize_params(
+    state: &mut TypeCheckState,
+    param_ids: impl IntoIterator<Item = NodeId<Param>>,
+) -> Result<ListId<NodeId<Param>>, TypeError> {
+    let normalized_param_ids: Vec<NodeId<Param>> = param_ids
+        .into_iter()
+        .map(|id| -> Result<NodeId<Param>, TypeError> {
+            let param = state.registry.param(id);
+            let param_type_id = param.type_id;
+            let normalized_param_with_dummy_id = Param {
+                id: dummy_id(),
+                is_dashed: param.is_dashed,
+                name_id: param.name_id,
+                // It's safe to call `evaluate_well_typed_expression`
+                // because we type-checked it above.
+                type_id: evaluate_well_typed_expression(state, param_type_id)?.0,
+            };
+            Ok(state
+                .registry
+                .add_param_and_overwrite_its_id(normalized_param_with_dummy_id))
+        })
+        .collect::<Result<Vec<_>, TypeError>>()?;
+    Ok(state.registry.add_param_list(normalized_param_ids))
+}
+
 fn are_types_equal(_state: &TypeCheckState, _a: NormalFormNodeId, _b: NormalFormNodeId) -> bool {
     unimplemented!()
 }
@@ -331,6 +361,7 @@ fn dummy_id<T>() -> NodeId<T> {
 struct TypeCheckState<'a> {
     registry: &'a mut NodeRegistry,
     symbol_db: &'a mut SymbolDatabase,
+    variant_return_type_args: &'a VariantReturnTypeTypeArgsMap,
     context: TypeCheckContext,
     type0_identifier_id: NormalFormNodeId,
 }
