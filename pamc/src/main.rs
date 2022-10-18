ba980aea7c0d6beef278d2ac77f56dbaa8f87d16
Kyle Lin <kylejlin.us@gmail.com>
2022-10-17 22:04:34 -0700

    Fix main.rs

--- pamc/src/main.rs
+++ pamc/src/main.rs
@@ -36,91 +36,113 @@ fn main() {
                     println!("{:#?}", file);
                     print_separator();
 
-                    let mut registry = pamc::data::node_registry::NodeRegistry::empty();
-                    let file_node_id =
-                        pamc::processing::register::register_file(&mut registry, file);
-                    println!("Registered file.");
-                    println!("{:#?}", registry.file(file_node_id));
-                    print_separator();
+                    let simplification_result = pamc::processing::simplify_ast::simplify_file(file);
+                    match simplification_result {
+                        Ok(file) => {
+                            println!("Simplification success!");
+                            println!("{:#?}", file);
+                            print_separator();
 
-                    let bind_result =
+                            let mut registry = pamc::data::node_registry::NodeRegistry::empty();
+                            let file_node_id =
+                                pamc::processing::register::register_file(&mut registry, file);
+                            println!("Registered file.");
+                            println!("{:#?}", registry.file(file_node_id));
+                            print_separator();
+
+                            let bind_result =
                         pamc::processing::bind_type_independent::bind_symbols_to_identifiers(
                             &registry,
                             vec![file_node_id],
                         );
-                    match bind_result {
-                        Ok(symbol_db) => {
-                            println!("Bind success!");
-                            println!("{:#?}", symbol_db.identifier_symbols);
-                            print_separator();
-                            let all_identifiers = registry.TODO_identifiers().to_vec();
-                            let mut unbound_identifiers = vec![];
-                            let mut bound_identifiers = vec![];
-                            for identifier in all_identifiers {
-                                if !symbol_db.identifier_symbols.contains(identifier.id) {
-                                    unbound_identifiers.push(identifier);
-                                } else {
-                                    bound_identifiers.push(identifier);
-                                }
-                            }
-                            println!("Unbound identifiers: {:#?}", unbound_identifiers);
-                            print_separator();
-                            let mut identifiers_with_undefined_symbols = vec![];
-                            for identifier in bound_identifiers {
-                                let symbol = symbol_db.identifier_symbols.get(identifier.id);
-                                if !symbol_db.symbol_sources.contains_key(&symbol) {
-                                    identifiers_with_undefined_symbols.push(identifier);
-                                }
-                            }
-                            println!(
-                                "Identifiers with undefined symbols: {:#?}",
-                                identifiers_with_undefined_symbols
-                            );
-                            print_separator();
+                            match bind_result {
+                                Ok(symbol_db) => {
+                                    println!("Bind success!");
+                                    println!("{:#?}", symbol_db.identifier_symbols);
+                                    print_separator();
+                                    let all_identifiers = registry.TODO_identifiers().to_vec();
+                                    let mut unbound_identifiers = vec![];
+                                    let mut bound_identifiers = vec![];
+                                    for identifier in all_identifiers {
+                                        if !symbol_db.identifier_symbols.contains(identifier.id) {
+                                            unbound_identifiers.push(identifier);
+                                        } else {
+                                            bound_identifiers.push(identifier);
+                                        }
+                                    }
+                                    println!("Unbound identifiers: {:#?}", unbound_identifiers);
+                                    print_separator();
+                                    let mut identifiers_with_undefined_symbols = vec![];
+                                    for identifier in bound_identifiers {
+                                        let symbol =
+                                            symbol_db.identifier_symbols.get(identifier.id);
+                                        if !symbol_db.symbol_sources.contains_key(&symbol) {
+                                            identifiers_with_undefined_symbols.push(identifier);
+                                        }
+                                    }
+                                    println!(
+                                        "Identifiers with undefined symbols: {:#?}",
+                                        identifiers_with_undefined_symbols
+                                    );
+                                    print_separator();
 
-                            let variant_return_type_validation_result = pamc::processing::check_variant_return_types::check_variant_return_types_for_file(&symbol_db, &registry, registry.file(file_node_id));
-                            match variant_return_type_validation_result {
-                                Ok(variant_db) => {
-                                    println!("Variant return type validation success!");
+                                    let variant_return_type_validation_result = pamc::processing::check_variant_return_types::check_variant_return_types_for_file(&symbol_db, &registry, registry.file(file_node_id));
+                                    match variant_return_type_validation_result {
+                                        Ok(variant_db) => {
+                                            println!("Variant return type validation success!");
 
-                                    let rec_validation_result = pamc::processing::validate_fun_recursion::validate_fun_recursion_in_file(
+                                            let rec_validation_result = pamc::processing::validate_fun_recursion::validate_fun_recursion_in_file(
                                         &symbol_db,
                                         &registry,
                                         registry.file(file_node_id),
                                     );
-                                    match rec_validation_result {
-                                        Ok(()) => {
-                                            println!("Recursion validation success!");
-                                            let mut symbol_db = symbol_db;
-                                            let type_check_result =
+                                            match rec_validation_result {
+                                                Ok(()) => {
+                                                    println!("Recursion validation success!");
+                                                    let mut symbol_db = symbol_db;
+                                                    let type_check_result =
                                                 pamc::processing::type_check::type_check_file(
                                                     &mut registry,
                                                     &mut symbol_db,
                                                     &variant_db,
                                                     file_node_id,
                                                 );
-                                            match type_check_result {
-                                                Ok(type_map) => {
-                                                    println!("Type check success!");
-                                                    println!("{:#?}", type_map);
+                                                    match type_check_result {
+                                                        Ok(type_map) => {
+                                                            println!("Type check success!");
+                                                            println!("{:#?}", type_map);
+                                                        }
+                                                        Err(err) => {
+                                                            println!(
+                                                                "Type check error: {:#?}",
+                                                                err
+                                                            );
+                                                        }
+                                                    }
                                                 }
                                                 Err(err) => {
-                                                    println!("Type check error: {:#?}", err);
+                                                    println!(
+                                                        "Recursion validation error: {:#?}",
+                                                        err
+                                                    );
                                                 }
                                             }
                                         }
-                                        Err(err) => {
-                                            println!("Recursion validation error: {:#?}", err);
+                                        Err(error) => {
+                                            println!(
+                                                "Variant return type validation error: {:#?}",
+                                                error
+                                            );
                                         }
                                     }
                                 }
-                                Err(error) => {
-                                    println!("Variant return type validation error: {:#?}", error);
+                                Err(e) => {
+                                    println!("Bind error: {:#?}", e);
                                 }
                             }
                         }
-                        Err(e) => {
-                            println!("Bind error: {:#?}", e);
+                        Err(err) => {
+                            println!("Simplification error: {:#?}", err);
                         }
                     }
                 }
