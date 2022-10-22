f9d4659239147c9b8fa5dbb2db332dc49f306088
Kyle Lin <kylejlin.us@gmail.com>
2022-10-22 00:17:29 -0700

    Add JS codegen tests

--- pamc/src/tests/should_succeed.rs
+++ pamc/src/tests/should_succeed.rs
@@ -2,8 +2,12 @@ use crate::{
     data::{node_registry::NodeRegistry, FileId},
     processing::{
         bind_type_independent::bind_symbols_to_identifiers,
-        check_variant_return_types::check_variant_return_types_for_file, lex::lex,
-        parse::parse_file, register::register_file, simplify_ast::simplify_file,
+        check_variant_return_types::check_variant_return_types_for_file,
+        generate_code::{targets::javascript::JavaScript, CompileTarget},
+        lex::lex,
+        parse::parse_file,
+        register::register_file,
+        simplify_ast::simplify_file,
         validate_fun_recursion::validate_fun_recursion_in_file,
     },
 };
@@ -29,8 +33,11 @@ fn expect_success(src: &str) {
     let file_id = register_file(&mut registry, file);
     let file = registry.file(file_id);
     let symbol_db = bind_symbols_to_identifiers(&registry, vec![file_id]).expect("Binding failed");
-    let _variant_return_type_map = check_variant_return_types_for_file(&symbol_db, &registry, file)
+    let variant_return_type_map = check_variant_return_types_for_file(&symbol_db, &registry, file)
         .expect("Extracting variant type args failed");
     validate_fun_recursion_in_file(&symbol_db, &registry, file)
         .expect("Fun recursion validation failed");
+    let _js_ast =
+        JavaScript::generate_code(&symbol_db, &registry, &variant_return_type_map, &[file_id])
+            .expect("Code generation failed");
 }
