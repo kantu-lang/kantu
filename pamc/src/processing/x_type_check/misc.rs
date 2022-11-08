34d898ed547568f56561c8c07f90ac5666a899bc
Kyle Lin <kylejlin.us@gmail.com>
2022-11-07 16:22:54 -0800

    Add todo comment

--- pamc/src/processing/x_type_check/misc.rs
+++ pamc/src/processing/x_type_check/misc.rs
@@ -590,6 +590,9 @@ pub(super) fn apply_dynamic_substitutions_with_compounding(
 }
 
 fn get_concrete_substitution(state: &mut State, d: DynamicSubstitution) -> Option<Substitution> {
+    // TODO: Consider comparing `d.0` and `d.1` for equality directly,
+    // since we may want to treat NameExpressions with same `db_index`s
+    // but different names as distinct, for the sake of sane error messages.
     if state
         .equality_checker
         .eq(d.0.raw(), d.1.raw(), state.registry)
