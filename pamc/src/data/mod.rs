e00c61928e1e66bdf8f782a8a8233bda30c5a91e
Kyle Lin <kylejlin.us@gmail.com>
2022-11-19 22:11:37 -0800

    Remove some unused files

--- pamc/src/data/mod.rs
+++ pamc/src/data/mod.rs
@@ -1,13 +1,10 @@
 pub mod bound_ast;
 pub mod light_ast;
 pub mod node_equality_checker;
-pub mod node_free_variable_cache;
-pub mod node_hash_cache;
 pub mod node_registry;
 pub mod simplified_ast;
 pub mod symbol_database;
 pub mod token;
-pub mod type_map;
 pub mod unsimplified_ast;
 
 pub mod x_light_ast;
