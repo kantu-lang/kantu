6bcb2ba9713b742bd4a6d29cbafb64807023e0ae
Kyle Lin <kylejlin.us@gmail.com>
2022-11-22 00:24:25 -0800

    Add missing match case test

--- pamc/src/tests/should_fail/type_check.rs
+++ pamc/src/tests/should_fail/type_check.rs
@@ -423,4 +423,24 @@ mod duplicate_match_case {
         expect_duplicate_match_case_error(src, "U => Bool.True,", "U => Bool.False,");
     }
 }
+
+mod missing_match_case {
+    use super::*;
+
+    fn expect_missing_match_case_error(src: &str, expected_variant_name: &IdentifierName) {
+        expect_type_check_error(src, |registry, err| match err {
+            TypeCheckError::MissingMatchCase { variant_name_id } => {
+                let actual_variant_name = &registry.identifier(variant_name_id).name;
+                assert_eq!(expected_variant_name, actual_variant_name);
+            }
+            _ => panic!("Unexpected error: {:#?}", err),
+        });
+    }
+
+    #[test]
+    fn missing_match_case() {
+        let src = include_str!("../sample_code/should_fail/type_check/missing_match_case.ph");
+        expect_missing_match_case_error(src, &standard_ident_name("False"));
+    }
+}
 // TODO: Add other tests
