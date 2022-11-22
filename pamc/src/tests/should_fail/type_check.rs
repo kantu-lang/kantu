5cebea5527a3ea73f1bfe36515d98fedc14e054a
Kyle Lin <kylejlin.us@gmail.com>
2022-11-21 23:17:42 -0800

    Reorganize test structure

--- pamc/src/tests/should_fail/type_check.rs
+++ pamc/src/tests/should_fail/type_check.rs
@@ -23,58 +23,65 @@ fn expect_recursion_error(src: &str, panicker: impl Fn(&NodeRegistry, TypeCheckE
     panicker(&registry, err);
 }
 
-fn expect_illegal_type_error(src: &str, expected_illegal_type_src: &str) {
-    expect_recursion_error(src, |registry, err| match err {
-        TypeCheckError::IllegalTypeExpression(id) => {
-            let actual_src = format_expression(
-                &expand_expression(registry, id),
-                0,
-                &FormatOptions {
-                    ident_size_in_spaces: 4,
-                    print_db_indices: false,
-                    print_fun_body_status: false,
-                },
-            );
-            assert_eq_up_to_white_space(&actual_src, expected_illegal_type_src);
-        }
-        _ => panic!("Unexpected error: {:#?}", err),
-    });
-}
+mod illegal_type {
+    use super::*;
 
-#[test]
-fn illegal_type_forall_output() {
-    let src = include_str!("../sample_code/should_fail/type_check/illegal_type/forall_output.ph");
-    expect_illegal_type_error(src, "U.U");
-}
+    fn expect_illegal_type_error(src: &str, expected_illegal_type_src: &str) {
+        expect_recursion_error(src, |registry, err| match err {
+            TypeCheckError::IllegalTypeExpression(id) => {
+                let actual_src = format_expression(
+                    &expand_expression(registry, id),
+                    0,
+                    &FormatOptions {
+                        ident_size_in_spaces: 4,
+                        print_db_indices: false,
+                        print_fun_body_status: false,
+                    },
+                );
+                assert_eq_up_to_white_space(&actual_src, expected_illegal_type_src);
+            }
+            _ => panic!("Unexpected error: {:#?}", err),
+        });
+    }
 
-#[test]
-fn illegal_type_forall_param() {
-    let src = include_str!("../sample_code/should_fail/type_check/illegal_type/forall_param.ph");
-    expect_illegal_type_error(src, "U.U");
-}
+    #[test]
+    fn illegal_type_forall_output() {
+        let src =
+            include_str!("../sample_code/should_fail/type_check/illegal_type/forall_output.ph");
+        expect_illegal_type_error(src, "U.U");
+    }
 
-#[test]
-fn illegal_type_fun_param() {
-    let src = include_str!("../sample_code/should_fail/type_check/illegal_type/fun_param.ph");
-    expect_illegal_type_error(src, "U.U");
-}
+    #[test]
+    fn illegal_type_forall_param() {
+        let src =
+            include_str!("../sample_code/should_fail/type_check/illegal_type/forall_param.ph");
+        expect_illegal_type_error(src, "U.U");
+    }
 
-#[test]
-fn illegal_type_fun_return() {
-    let src = include_str!("../sample_code/should_fail/type_check/illegal_type/fun_return.ph");
-    expect_illegal_type_error(src, "U.U");
-}
+    #[test]
+    fn illegal_type_fun_param() {
+        let src = include_str!("../sample_code/should_fail/type_check/illegal_type/fun_param.ph");
+        expect_illegal_type_error(src, "U.U");
+    }
 
-#[test]
-fn illegal_type_type_param() {
-    let src = include_str!("../sample_code/should_fail/type_check/illegal_type/type_param.ph");
-    expect_illegal_type_error(src, "U.U");
-}
+    #[test]
+    fn illegal_type_fun_return() {
+        let src = include_str!("../sample_code/should_fail/type_check/illegal_type/fun_return.ph");
+        expect_illegal_type_error(src, "U.U");
+    }
 
-#[test]
-fn illegal_type_variant_param() {
-    let src = include_str!("../sample_code/should_fail/type_check/illegal_type/variant_param.ph");
-    expect_illegal_type_error(src, "U.U");
+    #[test]
+    fn illegal_type_type_param() {
+        let src = include_str!("../sample_code/should_fail/type_check/illegal_type/type_param.ph");
+        expect_illegal_type_error(src, "U.U");
+    }
+
+    #[test]
+    fn illegal_type_variant_param() {
+        let src =
+            include_str!("../sample_code/should_fail/type_check/illegal_type/variant_param.ph");
+        expect_illegal_type_error(src, "U.U");
+    }
 }
 
 // TODO: Add other tests
