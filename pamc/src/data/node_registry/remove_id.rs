455073835eae8d424ca2fdc993e1a22f5aa414c6
Kyle Lin <kylejlin.us@gmail.com>
2022-12-01 22:04:30 -0800

    Add RemoveId::Output: AddId trait bound

--- pamc/src/data/node_registry/remove_id.rs
+++ pamc/src/data/node_registry/remove_id.rs
@@ -6,12 +6,20 @@ use crate::data::{
     simplified_ast as unbound, FileId, TextPosition,
 };
 
-// TODO: Force developer to implement reverse conversion
-// to ensure correctness.
-
 pub trait RemoveId {
-    type Output: Eq + std::hash::Hash;
+    type Output: Eq + std::hash::Hash + AddId<Output = Self>;
     fn remove_id(&self) -> Self::Output;
+}
+
+/// This trait isn't actually meant to be used.
+/// We just require it as a trait bound on `RemoveId::Output`
+/// to ensure that the only information lost is the id.
+/// In the past, bugs were caused when other fields were omitted
+/// when removing the ids, which caused nodes that were not equal
+/// (even modulo id) to be incorrectly considered equal (modulo id).
+pub trait AddId {
+    type Output;
+    fn add_id(&self, id: NodeId<Self::Output>) -> Self::Output;
 }
 
 #[derive(Clone, Debug, PartialEq, Eq, Hash)]
@@ -24,6 +32,16 @@ impl RemoveId for with_id::File {
     fn remove_id(&self) -> Self::Output {
         File {
             file_id: self.file_id,
+            item_list_id: self.item_list_id,
+        }
+    }
+}
+impl AddId for File {
+    type Output = with_id::File;
+    fn add_id(&self, id: NodeId<Self::Output>) -> Self::Output {
+        with_id::File {
+            file_id: self.file_id,
+            id,
             item_list_id: self.item_list_id,
         }
     }
@@ -47,6 +65,17 @@ impl RemoveId for with_id::TypeStatement {
         }
     }
 }
+impl AddId for TypeStatement {
+    type Output = with_id::TypeStatement;
+    fn add_id(&self, id: NodeId<Self::Output>) -> Self::Output {
+        with_id::TypeStatement {
+            id,
+            name_id: self.name_id,
+            param_list_id: self.param_list_id,
+            variant_list_id: self.variant_list_id,
+        }
+    }
+}
 
 #[derive(Clone, Debug, PartialEq, Eq, Hash)]
 pub struct Param {
@@ -58,6 +87,17 @@ impl RemoveId for with_id::Param {
     type Output = Param;
     fn remove_id(&self) -> Self::Output {
         Param {
+            is_dashed: self.is_dashed,
+            name_id: self.name_id,
+            type_id: self.type_id,
+        }
+    }
+}
+impl AddId for Param {
+    type Output = with_id::Param;
+    fn add_id(&self, id: NodeId<Self::Output>) -> Self::Output {
+        with_id::Param {
+            id,
             is_dashed: self.is_dashed,
             name_id: self.name_id,
             type_id: self.type_id,
@@ -81,6 +121,17 @@ impl RemoveId for with_id::Variant {
         }
     }
 }
+impl AddId for Variant {
+    type Output = with_id::Variant;
+    fn add_id(&self, id: NodeId<Self::Output>) -> Self::Output {
+        with_id::Variant {
+            id,
+            name_id: self.name_id,
+            param_list_id: self.param_list_id,
+            return_type_id: self.return_type_id,
+        }
+    }
+}
 
 #[derive(Clone, Debug, PartialEq, Eq, Hash)]
 pub struct LetStatement {
@@ -91,6 +142,16 @@ impl RemoveId for with_id::LetStatement {
     type Output = LetStatement;
     fn remove_id(&self) -> Self::Output {
         LetStatement {
+            name_id: self.name_id,
+            value_id: self.value_id,
+        }
+    }
+}
+impl AddId for LetStatement {
+    type Output = with_id::LetStatement;
+    fn add_id(&self, id: NodeId<Self::Output>) -> Self::Output {
+        with_id::LetStatement {
+            id,
             name_id: self.name_id,
             value_id: self.value_id,
         }
@@ -109,6 +170,16 @@ impl RemoveId for with_id::NameExpression {
     type Output = NameExpression;
     fn remove_id(&self) -> Self::Output {
         NameExpression {
+            component_list_id: self.component_list_id,
+            db_index: self.db_index,
+        }
+    }
+}
+impl AddId for NameExpression {
+    type Output = with_id::NameExpression;
+    fn add_id(&self, id: NodeId<Self::Output>) -> Self::Output {
+        with_id::NameExpression {
+            id,
             component_list_id: self.component_list_id,
             db_index: self.db_index,
         }
@@ -134,6 +205,16 @@ impl RemoveId for with_id::Identifier {
         }
     }
 }
+impl AddId for Identifier {
+    type Output = with_id::Identifier;
+    fn add_id(&self, id: NodeId<Self::Output>) -> Self::Output {
+        with_id::Identifier {
+            id,
+            start: self.start,
+            name: self.name.clone(),
+        }
+    }
+}
 
 pub use crate::data::simplified_ast::IdentifierName;
 
@@ -148,6 +229,16 @@ impl RemoveId for with_id::Call {
     type Output = Call;
     fn remove_id(&self) -> Self::Output {
         Call {
+            callee_id: self.callee_id,
+            arg_list_id: self.arg_list_id,
+        }
+    }
+}
+impl AddId for Call {
+    type Output = with_id::Call;
+    fn add_id(&self, id: NodeId<Self::Output>) -> Self::Output {
+        with_id::Call {
+            id,
             callee_id: self.callee_id,
             arg_list_id: self.arg_list_id,
         }
@@ -174,6 +265,19 @@ impl RemoveId for with_id::Fun {
         }
     }
 }
+impl AddId for Fun {
+    type Output = with_id::Fun;
+    fn add_id(&self, id: NodeId<Self::Output>) -> Self::Output {
+        with_id::Fun {
+            id,
+            name_id: self.name_id,
+            param_list_id: self.param_list_id,
+            return_type_id: self.return_type_id,
+            body_id: self.body_id,
+            skip_type_checking_body: self.skip_type_checking_body,
+        }
+    }
+}
 
 #[derive(Clone, Debug, PartialEq, Eq, Hash)]
 pub struct Match {
@@ -184,6 +288,16 @@ impl RemoveId for with_id::Match {
     type Output = Match;
     fn remove_id(&self) -> Self::Output {
         Match {
+            matchee_id: self.matchee_id,
+            case_list_id: self.case_list_id,
+        }
+    }
+}
+impl AddId for Match {
+    type Output = with_id::Match;
+    fn add_id(&self, id: NodeId<Self::Output>) -> Self::Output {
+        with_id::Match {
+            id,
             matchee_id: self.matchee_id,
             case_list_id: self.case_list_id,
         }
@@ -206,7 +320,17 @@ impl RemoveId for with_id::MatchCase {
         }
     }
 }
-
+impl AddId for MatchCase {
+    type Output = with_id::MatchCase;
+    fn add_id(&self, id: NodeId<Self::Output>) -> Self::Output {
+        with_id::MatchCase {
+            id,
+            variant_name_id: self.variant_name_id,
+            param_list_id: self.param_list_id,
+            output_id: self.output_id,
+        }
+    }
+}
 #[derive(Clone, Debug, PartialEq, Eq, Hash)]
 pub struct Forall {
     pub param_list_id: ListId<NodeId<with_id::Param>>,
@@ -216,6 +340,16 @@ impl RemoveId for with_id::Forall {
     type Output = Forall;
     fn remove_id(&self) -> Self::Output {
         Forall {
+            param_list_id: self.param_list_id,
+            output_id: self.output_id,
+        }
+    }
+}
+impl AddId for Forall {
+    type Output = with_id::Forall;
+    fn add_id(&self, id: NodeId<Self::Output>) -> Self::Output {
+        with_id::Forall {
+            id,
             param_list_id: self.param_list_id,
             output_id: self.output_id,
         }
@@ -236,6 +370,16 @@ impl RemoveId for with_id::Check {
         }
     }
 }
+impl AddId for Check {
+    type Output = with_id::Check;
+    fn add_id(&self, id: NodeId<Self::Output>) -> Self::Output {
+        with_id::Check {
+            id,
+            checkee_annotation_id: self.checkee_annotation_id,
+            output_id: self.output_id,
+        }
+    }
+}
 
 #[derive(Clone, Debug, PartialEq, Eq, Hash)]
 pub struct GoalCheckeeAnnotation {
@@ -246,6 +390,16 @@ impl RemoveId for with_id::GoalCheckeeAnnotation {
     type Output = GoalCheckeeAnnotation;
     fn remove_id(&self) -> Self::Output {
         GoalCheckeeAnnotation {
+            goal_kw_position: self.goal_kw_position,
+            checkee_type_id: self.checkee_type_id,
+        }
+    }
+}
+impl AddId for GoalCheckeeAnnotation {
+    type Output = with_id::GoalCheckeeAnnotation;
+    fn add_id(&self, id: NodeId<Self::Output>) -> Self::Output {
+        with_id::GoalCheckeeAnnotation {
+            id,
             goal_kw_position: self.goal_kw_position,
             checkee_type_id: self.checkee_type_id,
         }
@@ -268,6 +422,17 @@ impl RemoveId for with_id::ExpressionCheckeeAnnotation {
         }
     }
 }
+impl AddId for ExpressionCheckeeAnnotation {
+    type Output = with_id::ExpressionCheckeeAnnotation;
+    fn add_id(&self, id: NodeId<Self::Output>) -> Self::Output {
+        with_id::ExpressionCheckeeAnnotation {
+            id,
+            checkee_id: self.checkee_id,
+            checkee_type_id: self.checkee_type_id,
+            checkee_value_id: self.checkee_value_id,
+        }
+    }
+}
 
 #[derive(Clone, Debug, PartialEq, Eq, Hash)]
 pub struct UnbindableExpression {
@@ -283,6 +448,16 @@ impl RemoveId for with_id::UnbindableExpression {
         }
     }
 }
+impl AddId for UnbindableExpression {
+    type Output = with_id::UnbindableExpression;
+    fn add_id(&self, id: NodeId<Self::Output>) -> Self::Output {
+        with_id::UnbindableExpression {
+            id,
+            expression: self.expression.clone(),
+            error: self.error.clone(),
+        }
+    }
+}
 
 #[derive(Clone, Debug, PartialEq, Eq, Hash)]
 pub struct IllegalFunRecursionExpression {
@@ -293,6 +468,16 @@ impl RemoveId for with_id::IllegalFunRecursionExpression {
     type Output = IllegalFunRecursionExpression;
     fn remove_id(&self) -> Self::Output {
         IllegalFunRecursionExpression {
+            expression_id: self.expression_id.clone(),
+            error: self.error.clone(),
+        }
+    }
+}
+impl AddId for IllegalFunRecursionExpression {
+    type Output = with_id::IllegalFunRecursionExpression;
+    fn add_id(&self, id: NodeId<Self::Output>) -> Self::Output {
+        with_id::IllegalFunRecursionExpression {
+            id,
             expression_id: self.expression_id.clone(),
             error: self.error.clone(),
         }
