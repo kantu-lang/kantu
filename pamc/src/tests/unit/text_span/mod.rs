ae8496aac33b42a6bd3165915d9df03a935ff2cb
Kyle Lin <kylejlin.us@gmail.com>
2022-12-11 21:55:00 -0800

    Add more text span tests

--- pamc/src/tests/unit/text_span/mod.rs
+++ pamc/src/tests/unit/text_span/mod.rs
@@ -24,3 +24,56 @@ fn hello_world() {
     let src = include_str!("../../sample_code/should_succeed/hello_world.ph");
     verify_that_spans_are_correct(src);
 }
+
+#[test]
+fn ill_typed_until_substituted() {
+    let src = include_str!("../../sample_code/should_succeed/ill_typed_until_substituted.ph");
+    verify_that_spans_are_correct(src);
+}
+
+#[test]
+fn plus_commutative() {
+    let src = include_str!("../../sample_code/should_succeed/plus_commutative.ph");
+    verify_that_spans_are_correct(src);
+}
+
+#[test]
+fn forall() {
+    let src = include_str!("../../sample_code/should_succeed/forall.ph");
+    verify_that_spans_are_correct(src);
+}
+
+#[test]
+fn exists() {
+    let src = include_str!("../../sample_code/should_succeed/exists.ph");
+    verify_that_spans_are_correct(src);
+}
+#[test]
+fn check() {
+    let src = include_str!("../../sample_code/should_succeed/check.ph");
+    verify_that_spans_are_correct(src);
+}
+
+#[test]
+fn comment() {
+    let src = include_str!("../../sample_code/should_succeed/comment.ph");
+    verify_that_spans_are_correct(src);
+}
+
+#[test]
+fn match_explosion() {
+    let src = include_str!("../../sample_code/should_succeed/match_explosion.ph");
+    verify_that_spans_are_correct(src);
+}
+
+#[test]
+fn underscore() {
+    let src = include_str!("../../sample_code/should_succeed/underscore.ph");
+    verify_that_spans_are_correct(src);
+}
+
+#[test]
+fn optional_commas() {
+    let src = include_str!("../../sample_code/should_succeed/optional_commas.ph");
+    verify_that_spans_are_correct(src);
+}
