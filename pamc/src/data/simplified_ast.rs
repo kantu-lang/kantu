833c2be2e5996d44529317d5a6b6eec807fac853
Kyle Lin <kylejlin.us@gmail.com>
2022-12-12 10:48:46 -0800

    Properly simplify check expressions

--- pamc/src/data/simplified_ast.rs
+++ pamc/src/data/simplified_ast.rs
@@ -2,6 +2,7 @@ use crate::data::{FileId, TextSpan};
 
 #[derive(Clone, Debug, PartialEq, Eq, Hash)]
 pub struct File {
+    pub span: TextSpan,
     pub id: FileId,
     pub items: Vec<FileItem>,
 }
@@ -12,8 +13,18 @@ pub enum FileItem {
     Let(LetStatement),
 }
 
+impl FileItem {
+    pub fn span(&self) -> TextSpan {
+        match self {
+            FileItem::Type(type_) => type_.span,
+            FileItem::Let(let_) => let_.span,
+        }
+    }
+}
+
 #[derive(Clone, Debug, PartialEq, Eq, Hash)]
 pub struct TypeStatement {
+    pub span: TextSpan,
     pub name: Identifier,
     pub params: Vec<Param>,
     pub variants: Vec<Variant>,
@@ -21,6 +32,7 @@ pub struct TypeStatement {
 
 #[derive(Clone, Debug, PartialEq, Eq, Hash)]
 pub struct Param {
+    pub span: TextSpan,
     pub is_dashed: bool,
     pub name: Identifier,
     pub type_: Expression,
@@ -28,6 +40,7 @@ pub struct Param {
 
 #[derive(Clone, Debug, PartialEq, Eq, Hash)]
 pub struct Variant {
+    pub span: TextSpan,
     pub name: Identifier,
     pub params: Vec<Param>,
     pub return_type: Expression,
@@ -35,6 +48,7 @@ pub struct Variant {
 
 #[derive(Clone, Debug, PartialEq, Eq, Hash)]
 pub struct LetStatement {
+    pub span: TextSpan,
     pub name: Identifier,
     pub value: Expression,
 }
@@ -49,8 +63,22 @@ pub enum Expression {
     Check(Box<Check>),
 }
 
+impl Expression {
+    pub fn span(&self) -> TextSpan {
+        match self {
+            Expression::Name(name) => name.span,
+            Expression::Call(call) => call.span,
+            Expression::Fun(fun) => fun.span,
+            Expression::Match(match_) => match_.span,
+            Expression::Forall(forall) => forall.span,
+            Expression::Check(check) => check.span,
+        }
+    }
+}
+
 #[derive(Clone, Debug, PartialEq, Eq, Hash)]
 pub struct NameExpression {
+    pub span: TextSpan,
     pub components: Vec<Identifier>,
 }
 
@@ -62,12 +90,14 @@ pub use crate::data::unsimplified_ast::ReservedIdentifierName;
 
 #[derive(Clone, Debug, PartialEq, Eq, Hash)]
 pub struct Call {
+    pub span: TextSpan,
     pub callee: Expression,
     pub args: Vec<Expression>,
 }
 
 #[derive(Clone, Debug, PartialEq, Eq, Hash)]
 pub struct Fun {
+    pub span: TextSpan,
     pub name: Identifier,
     pub params: Vec<Param>,
     pub return_type: Expression,
@@ -76,12 +106,14 @@ pub struct Fun {
 
 #[derive(Clone, Debug, PartialEq, Eq, Hash)]
 pub struct Match {
+    pub span: TextSpan,
     pub matchee: Expression,
     pub cases: Vec<MatchCase>,
 }
 
 #[derive(Clone, Debug, PartialEq, Eq, Hash)]
 pub struct MatchCase {
+    pub span: TextSpan,
     pub variant_name: Identifier,
     pub params: Vec<Identifier>,
     pub output: Expression,
@@ -89,37 +121,73 @@ pub struct MatchCase {
 
 #[derive(Clone, Debug, PartialEq, Eq, Hash)]
 pub struct Forall {
+    pub span: TextSpan,
     pub params: Vec<Param>,
     pub output: Expression,
 }
 
 #[derive(Clone, Debug, PartialEq, Eq, Hash)]
 pub struct Check {
-    pub checkee_annotation: CheckeeAnnotation,
+    pub span: TextSpan,
+    pub assertions: Vec<CheckAssertion>,
     pub output: Expression,
 }
 
 #[derive(Clone, Debug, PartialEq, Eq, Hash)]
-pub enum CheckeeAnnotation {
-    Goal(GoalCheckeeAnnotation),
-    Expression(ExpressionCheckeeAnnotation),
+pub enum CheckAssertion {
+    Type(TypeAssertion),
+    NormalForm(NormalFormAssertion),
+}
+
+impl CheckAssertion {
+    pub fn span(&self) -> TextSpan {
+        match self {
+            CheckAssertion::Type(type_) => type_.span,
+            CheckAssertion::NormalForm(normal_form) => normal_form.span,
+        }
+    }
 }
 
 #[derive(Clone, Debug, PartialEq, Eq, Hash)]
-pub struct GoalCheckeeAnnotation {
-    pub goal_kw_span: TextSpan,
-    pub checkee_type: QuestionMarkOrExpression,
+pub struct TypeAssertion {
+    pub span: TextSpan,
+    pub left: Expression,
+    pub right: QuestionMarkOrExpression,
 }
 
 #[derive(Clone, Debug, PartialEq, Eq, Hash)]
-pub struct ExpressionCheckeeAnnotation {
-    pub checkee: Expression,
-    pub checkee_type: QuestionMarkOrExpression,
-    pub checkee_value: Option<QuestionMarkOrExpression>,
+pub struct NormalFormAssertion {
+    pub span: TextSpan,
+    pub left: GoalKwOrExpression,
+    pub right: QuestionMarkOrExpression,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
+pub enum GoalKwOrExpression {
+    GoalKw { span: TextSpan },
+    Expression(Expression),
+}
+
+impl GoalKwOrExpression {
+    pub fn span(&self) -> TextSpan {
+        match self {
+            GoalKwOrExpression::GoalKw { span } => *span,
+            GoalKwOrExpression::Expression(expression) => expression.span(),
+        }
+    }
 }
 
 #[derive(Clone, Debug, PartialEq, Eq, Hash)]
 pub enum QuestionMarkOrExpression {
     QuestionMark { span: TextSpan },
     Expression(Expression),
+}
+
+impl QuestionMarkOrExpression {
+    pub fn span(&self) -> TextSpan {
+        match self {
+            QuestionMarkOrExpression::QuestionMark { span } => *span,
+            QuestionMarkOrExpression::Expression(expression) => expression.span(),
+        }
+    }
 }
--- pamc/src/processing/simplify_ast.rs
+++ pamc/src/processing/simplify_ast.rs
@@ -11,6 +11,7 @@ pub enum SimplifyAstError {
 
 pub fn simplify_file(unsimplified: ust::File) -> Result<File, SimplifyAstError> {
     Ok(File {
+        span: unsimplified.span,
         id: unsimplified.id,
         items: vec_result_map(unsimplified.items, simplify_file_item)?,
     })
@@ -40,6 +41,7 @@ fn simplify_type_statement(
     unsimplified: ust::TypeStatement,
 ) -> Result<TypeStatement, SimplifyAstError> {
     Ok(TypeStatement {
+        span: unsimplified.span,
         name: unsimplified.name,
         params: vec_result_map(unsimplified.params, simplify_param)?,
         variants: vec_result_map(unsimplified.variants, simplify_variant)?,
@@ -48,6 +50,7 @@ fn simplify_type_statement(
 
 fn simplify_param(unsimplified: ust::Param) -> Result<Param, SimplifyAstError> {
     Ok(Param {
+        span: unsimplified.span,
         is_dashed: unsimplified.is_dashed,
         name: unsimplified.name,
         type_: simplify_expression(unsimplified.type_)?,
@@ -56,6 +59,7 @@ fn simplify_param(unsimplified: ust::Param) -> Result<Param, SimplifyAstError> {
 
 fn simplify_variant(unsimplified: ust::Variant) -> Result<Variant, SimplifyAstError> {
     Ok(Variant {
+        span: unsimplified.span,
         name: unsimplified.name,
         params: vec_result_map(unsimplified.params, simplify_param)?,
         return_type: simplify_expression(unsimplified.return_type)?,
@@ -66,6 +70,7 @@ fn simplify_let_statement(
     unsimplified: ust::LetStatement,
 ) -> Result<LetStatement, SimplifyAstError> {
     Ok(LetStatement {
+        span: unsimplified.span,
         name: unsimplified.name,
         value: simplify_expression(unsimplified.value)?,
     })
@@ -86,6 +91,7 @@ fn simplify_expression(unsimplified: ust::Expression) -> Result<Expression, Simp
 
 fn simplify_identifier(unsimplified: ust::Identifier) -> Expression {
     Expression::Name(NameExpression {
+        span: unsimplified.span,
         components: vec![unsimplified],
     })
 }
@@ -109,6 +115,7 @@ fn simplify_dot(unsimplified: Box<ust::Dot>) -> Result<Expression, SimplifyAstEr
     }
 
     Ok(Expression::Name(NameExpression {
+        span: unsimplified.span,
         components: get_components(ust::Expression::Dot(unsimplified))
             .map_err(|err| SimplifyAstError::IllegalDotLhs(err.0))?,
     }))
@@ -116,6 +123,7 @@ fn simplify_dot(unsimplified: Box<ust::Dot>) -> Result<Expression, SimplifyAstEr
 
 fn simplify_call(unsimplified: ust::Call) -> Result<Expression, SimplifyAstError> {
     Ok(Expression::Call(Box::new(Call {
+        span: unsimplified.span,
         callee: simplify_expression(unsimplified.callee)?,
         args: vec_result_map(unsimplified.args, simplify_expression)?,
     })))
@@ -123,6 +131,7 @@ fn simplify_call(unsimplified: ust::Call) -> Result<Expression, SimplifyAstError
 
 fn simplify_fun(unsimplified: ust::Fun) -> Result<Expression, SimplifyAstError> {
     Ok(Expression::Fun(Box::new(Fun {
+        span: unsimplified.span,
         name: unsimplified.name,
         params: vec_result_map(unsimplified.params, simplify_param)?,
         return_type: simplify_expression(unsimplified.return_type)?,
@@ -132,6 +141,7 @@ fn simplify_fun(unsimplified: ust::Fun) -> Result<Expression, SimplifyAstError> 
 
 fn simplify_match(unsimplified: ust::Match) -> Result<Expression, SimplifyAstError> {
     Ok(Expression::Match(Box::new(Match {
+        span: unsimplified.span,
         matchee: simplify_expression(unsimplified.matchee)?,
         cases: vec_result_map(unsimplified.cases, simplify_match_case)?,
     })))
@@ -139,6 +149,7 @@ fn simplify_match(unsimplified: ust::Match) -> Result<Expression, SimplifyAstErr
 
 fn simplify_match_case(unsimplified: ust::MatchCase) -> Result<MatchCase, SimplifyAstError> {
     Ok(MatchCase {
+        span: unsimplified.span,
         variant_name: unsimplified.variant_name,
         params: unsimplified.params,
         output: simplify_expression(unsimplified.output)?,
@@ -147,12 +158,74 @@ fn simplify_match_case(unsimplified: ust::MatchCase) -> Result<MatchCase, Simpli
 
 fn simplify_forall(unsimplified: ust::Forall) -> Result<Expression, SimplifyAstError> {
     Ok(Expression::Forall(Box::new(Forall {
+        span: unsimplified.span,
         params: vec_result_map(unsimplified.params, simplify_param)?,
         output: simplify_expression(unsimplified.output)?,
     })))
 }
 
 fn simplify_check(unsimplified: ust::Check) -> Result<Expression, SimplifyAstError> {
-    // TODO: Properly simplify Check
-    simplify_expression(unsimplified.output)
+    Ok(Expression::Check(Box::new(Check {
+        span: unsimplified.span,
+        assertions: vec_result_map(unsimplified.assertions, simplify_check_assertion)?,
+        output: simplify_expression(unsimplified.output)?,
+    })))
+}
+
+fn simplify_check_assertion(
+    unsimplified: ust::CheckAssertion,
+) -> Result<CheckAssertion, SimplifyAstError> {
+    Ok(match unsimplified {
+        ust::CheckAssertion::Type(assertion) => {
+            CheckAssertion::Type(simplify_type_assertion(assertion)?)
+        }
+
+        ust::CheckAssertion::NormalForm(assertion) => {
+            CheckAssertion::NormalForm(simplify_normal_form_assertion(assertion)?)
+        }
+    })
+}
+
+fn simplify_type_assertion(
+    unsimplified: ust::TypeAssertion,
+) -> Result<TypeAssertion, SimplifyAstError> {
+    Ok(TypeAssertion {
+        span: unsimplified.span,
+        left: simplify_expression(unsimplified.left)?,
+        right: simplify_question_mark_or_expression(unsimplified.right)?,
+    })
+}
+
+fn simplify_normal_form_assertion(
+    unsimplified: ust::NormalFormAssertion,
+) -> Result<NormalFormAssertion, SimplifyAstError> {
+    Ok(NormalFormAssertion {
+        span: unsimplified.span,
+        left: simplify_goal_kw_or_expression(unsimplified.left)?,
+        right: simplify_question_mark_or_expression(unsimplified.right)?,
+    })
+}
+
+fn simplify_question_mark_or_expression(
+    unsimplified: ust::QuestionMarkOrExpression,
+) -> Result<QuestionMarkOrExpression, SimplifyAstError> {
+    Ok(match unsimplified {
+        ust::QuestionMarkOrExpression::QuestionMark { span } => {
+            QuestionMarkOrExpression::QuestionMark { span }
+        }
+        ust::QuestionMarkOrExpression::Expression(expr) => {
+            QuestionMarkOrExpression::Expression(simplify_expression(expr)?)
+        }
+    })
+}
+
+fn simplify_goal_kw_or_expression(
+    unsimplified: ust::GoalKwOrExpression,
+) -> Result<GoalKwOrExpression, SimplifyAstError> {
+    Ok(match unsimplified {
+        ust::GoalKwOrExpression::GoalKw { span } => GoalKwOrExpression::GoalKw { span },
+        ust::GoalKwOrExpression::Expression(expr) => {
+            GoalKwOrExpression::Expression(simplify_expression(expr)?)
+        }
+    })
 }
