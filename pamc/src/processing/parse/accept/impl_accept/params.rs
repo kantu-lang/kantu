df39100713f684036dcab309279e538f20b1e903
Kyle Lin <kylejlin.us@gmail.com>
2022-12-17 17:11:10 -0800

    SQUASH: Remove some commented-out code

--- pamc/src/processing/parse/accept/impl_accept/params.rs
+++ pamc/src/processing/parse/accept/impl_accept/params.rs
@@ -1,45 +1,9 @@
 use super::*;
 
 impl Accept for UnfinishedParams {
-    fn accept(&mut self, item: FinishedStackItem, file_id: FileId) -> AcceptResult {
+    fn accept(&mut self, item: FinishedStackItem, _: FileId) -> AcceptResult {
         match item {
             FinishedStackItem::Token(token) => match token.kind {
-                // TODO: Delete
-                // TokenKind::Tilde => {
-                //     if self.pending_dash.is_some() {
-                //         // A tilde can never come after a dash.
-                //         AcceptResult::Error(ParseError::unexpected_token(token))
-                //     } else if self.pending_tilde.is_some() {
-                //         // Double tildes are forbidden.
-                //         AcceptResult::Error(ParseError::unexpected_token(token))
-                //     } else {
-                //         self.pending_tilde = Some(token);
-                //         AcceptResult::ContinueToNextToken
-                //     }
-                // }
-                // TokenKind::Dash => {
-                //     if self.maximum_dashed_params_allowed == 0 || self.pending_dash.is_some() {
-                //         AcceptResult::Error(ParseError::unexpected_token(token))
-                //     } else {
-                //         self.maximum_dashed_params_allowed -= 1;
-                //         self.pending_dash = Some(token);
-                //         AcceptResult::ContinueToNextToken
-                //     }
-                // }
-                // TokenKind::StandardIdentifier => {
-                //     let name_or_label = Identifier {
-                //         span: span_single(file_id, &token),
-                //         name: IdentifierName::Standard(token.content.clone()),
-                //     };
-                //     self.push_unfinished_param(name_or_label, token)
-                // }
-                // TokenKind::Underscore => {
-                //     let name_or_label = Identifier {
-                //         span: span_single(file_id, &token),
-                //         name: IdentifierName::Reserved(ReservedIdentifierName::Underscore),
-                //     };
-                //     self.push_unfinished_param(name_or_label, token)
-                // }
                 TokenKind::Tilde
                 | TokenKind::Dash
                 | TokenKind::StandardIdentifier
@@ -93,27 +57,3 @@ impl Accept for UnfinishedParams {
         }
     }
 }
-
-// TODO: Delete
-// impl UnfinishedParams {
-//     fn push_unfinished_param(
-//         &mut self,
-//         name_or_label: Identifier,
-//         name_or_label_token: Token,
-//     ) -> AcceptResult {
-//         let pending_tilde = self.pending_tilde.take();
-//         let pending_dash = self.pending_dash.take();
-//         let is_tilded = pending_tilde.is_some();
-//         let is_dashed = pending_dash.is_some();
-//         AcceptResult::Push(UnfinishedStackItem::Param(
-//             UnfinishedParam::FirstIdentifier {
-//                 first_token: pending_tilde
-//                     .unwrap_or_else(|| pending_dash.unwrap_or_else(|| name_or_label_token)),
-//                 is_tilded,
-//                 is_dashed,
-//                 is_dash_allowed: self.maximum_dashed_params_allowed > 0 || is_dashed,
-//                 name_or_label,
-//             },
-//         ))
-//     }
-// }
