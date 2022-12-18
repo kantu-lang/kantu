b01e6428b6f63d2fdc48b35a01a0ac6ae032d4d7
Kyle Lin <kylejlin.us@gmail.com>
2022-12-18 01:56:22 -0800

    Test match case param simplification
    Closes #29

--- pamc/src/tests/integration/should_fail/ast_simplification.rs
+++ pamc/src/tests/integration/should_fail/ast_simplification.rs
@@ -136,3 +136,114 @@ mod labeled_params {
         }
     }
 }
+
+mod labeled_match_case_params {
+    use super::*;
+
+    mod heterogeneous {
+        use super::*;
+
+        #[test]
+        fn explicitly_labeled_before_unlabeled_param() {
+            let src = include_str!("../../sample_code/should_fail/ast_simplification/labeled_match_case_params/heterogeneous/explicit_before_unlabeled.ph");
+            expect_heterogeneous_params_error(src);
+        }
+
+        #[test]
+        fn implicit_before_unlabeled() {
+            let src = include_str!("../../sample_code/should_fail/ast_simplification/labeled_match_case_params/heterogeneous/implicit_before_unlabeled.ph");
+            expect_heterogeneous_params_error(src);
+        }
+
+        #[test]
+        fn unlabeled_before_explicit() {
+            let src = include_str!("../../sample_code/should_fail/ast_simplification/labeled_match_case_params/heterogeneous/unlabeled_before_explicit.ph");
+            expect_heterogeneous_params_error(src);
+        }
+
+        #[test]
+        fn unlabeled_before_implicit() {
+            let src = include_str!("../../sample_code/should_fail/ast_simplification/labeled_match_case_params/heterogeneous/unlabeled_before_implicit.ph");
+            expect_heterogeneous_params_error(src);
+        }
+
+        fn expect_heterogeneous_params_error(src: &str) {
+            expect_simplification_error(src, |err| {
+                if !matches!(&err, SimplifyAstError::HeterogeneousMatchCaseParams(_)) {
+                    panic!("Unexpected error: {:#?}", err);
+                }
+            });
+        }
+    }
+
+    mod underscore_label {
+        use super::*;
+
+        #[test]
+        fn explicit_underscore_label() {
+            let src = include_str!("../../sample_code/should_fail/ast_simplification/labeled_match_case_params/underscore_label/explicit_underscore_label.ph");
+            expect_underscore_label_params_error(src);
+        }
+
+        #[test]
+        fn implicit_underscore_label() {
+            let src = include_str!("../../sample_code/should_fail/ast_simplification/labeled_match_case_params/underscore_label/implicit_underscore_label.ph");
+            expect_underscore_label_params_error(src);
+        }
+
+        #[test]
+        fn second_label_is_underscore() {
+            let src = include_str!("../../sample_code/should_fail/ast_simplification/labeled_match_case_params/underscore_label/second_label_is_underscore.ph");
+            expect_underscore_label_params_error(src);
+        }
+
+        fn expect_underscore_label_params_error(src: &str) {
+            expect_simplification_error(src, |err| {
+                if !matches!(&err, SimplifyAstError::UnderscoreMatchCaseParamLabel(_)) {
+                    panic!("Unexpected error: {:#?}", err);
+                }
+            });
+        }
+    }
+
+    mod duplicate_labels {
+        use super::*;
+
+        #[test]
+        fn explicit() {
+            let src = include_str!("../../sample_code/should_fail/ast_simplification/labeled_match_case_params/duplicate_labels/explicit.ph");
+            expect_duplicate_label_params_error(src, "pred");
+        }
+
+        #[test]
+        fn implicit() {
+            let src = include_str!("../../sample_code/should_fail/ast_simplification/labeled_match_case_params/duplicate_labels/implicit.ph");
+            expect_duplicate_label_params_error(src, "pred");
+        }
+
+        #[test]
+        fn implicit_explicit() {
+            let src = include_str!("../../sample_code/should_fail/ast_simplification/labeled_match_case_params/duplicate_labels/implicit_explicit.ph");
+            expect_duplicate_label_params_error(src, "pred");
+        }
+
+        #[test]
+        fn explicit_implicit() {
+            let src = include_str!("../../sample_code/should_fail/ast_simplification/labeled_match_case_params/duplicate_labels/explicit_implicit.ph");
+            expect_duplicate_label_params_error(src, "pred");
+        }
+
+        fn expect_duplicate_label_params_error(src: &str, label: &str) {
+            expect_simplification_error(src, |err| {
+                if let SimplifyAstError::DuplicateMatchCaseParamLabel(param1, param2) = &err {
+                    let label = standard_ident_name(label);
+                    let label = Some(&label);
+                    assert_eq!(label, param1.label_name());
+                    assert_eq!(label, param2.label_name());
+                } else {
+                    panic!("Unexpected error: {:#?}", err);
+                }
+            });
+        }
+    }
+}
---
+++ pamc/src/tests/sample_code/should_fail/ast_simplification/labeled_match_case_params/heterogeneous/unlabeled_before_explicit.ph
@@ -0,0 +1,11 @@
+type Nat {
+    .O: Nat,
+    .S(pred~n: Nat): Nat,
+}
+
+let pred = fun _(x: Nat): Nat {
+    match x {
+        .O => Nat.O,
+        .S(pred, pred: x) => x,
+    }
+};
