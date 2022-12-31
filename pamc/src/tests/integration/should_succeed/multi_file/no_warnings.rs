d304800ce5986d8250b12600b775ab94a2a95657
Kyle Lin <kylejlin.us@gmail.com>
2022-12-30 23:39:27 -0800

    Automate sample code file tree building

--- pamc/src/tests/integration/should_succeed/multi_file/no_warnings.rs
+++ pamc/src/tests/integration/should_succeed/multi_file/no_warnings.rs
@@ -1,42 +1,50 @@
 use super::*;
 
-#[derive(Clone, Debug)]
-struct FileBuilder<'a> {
-    id: FileId,
-    src: &'a str,
+use std::{
+    fs,
+    path::{Path, PathBuf},
+};
+
+/// This macro takes a string literal as input.
+///
+/// If the string literal is a valid path to a file, it returns
+/// the string literal as-is.
+///
+/// If the string literal is not a valid path to a file, it will
+/// cause a compilation error.
+#[macro_export]
+macro_rules! checked_path {
+    ($path_:literal) => {{
+        let _ = include_str!($path_);
+        $path_
+    }};
 }
 
-struct FileTreeBuilder<'a> {
-    root: FileId,
-    edges_to_children: Vec<(FileId, &'a str, FileId)>,
-}
-
-impl FileTreeBuilder<'_> {
-    fn build(self) -> FileTree {
-        let mut file_tree = FileTree::from_root(self.root);
-        for (parent, name, child) in self.edges_to_children {
-            let name = IdentifierName::new(name.to_string());
-            file_tree
-                .add_child(parent, &name, child)
-                .expect("Failed to add child");
-        }
-        file_tree
-    }
-}
-
-fn expect_success_with_no_warnings(
-    file_tree_builder: FileTreeBuilder,
-    file_builders: Vec<FileBuilder>,
-) {
-    let file_tree = file_tree_builder.build();
-    let files = file_builders
-        .into_iter()
-        .map(|builder| {
-            let tokens = lex(builder.src).expect("Lexing failed");
-            let file = parse_file(tokens, builder.id).expect("Parsing failed");
-            simplify_file(file).expect("AST Simplification failed")
-        })
-        .collect();
+fn expect_success_with_no_warnings(checked_unadjusted_pack_omlet_path: &str) {
+    let adjusted_pack_omlet_path = Path::new(file!())
+        .parent()
+        .unwrap()
+        .join(checked_unadjusted_pack_omlet_path);
+    let (root_file, root_file_path) = {
+        let root_file_path = adjusted_pack_omlet_path
+            .parent()
+            .unwrap()
+            .join("src/mod.ph");
+        let src = fs::read_to_string(&root_file_path).expect("Failed to open file");
+        let root_file = lex_and_parse_file(&src, FileId(0));
+        (root_file, root_file_path)
+    };
+    let (files, file_tree) = {
+        let root_file_id = root_file.id;
+        let mut files_and_paths = vec![(root_file, root_file_path)];
+        let mut file_tree = FileTree::from_root(root_file_id);
+        parse_children_then_add(&mut files_and_paths, &mut file_tree, root_file_id);
+        let files = files_and_paths
+            .into_iter()
+            .map(|(file, _)| file)
+            .collect::<Vec<_>>();
+        (files, file_tree)
+    };
     let file_items = bind_files(file_tree.root(), files, &file_tree).expect("Binding failed");
     let mut registry = NodeRegistry::empty();
     let file_item_list_id = register_file_items(&mut registry, file_items);
@@ -56,42 +64,77 @@ fn expect_success_with_no_warnings(
         .expect("Code generation failed");
 }
 
+fn lex_and_parse_file(src: &str, id: FileId) -> simplified_ast::File {
+    let tokens = lex(src).expect("Lexing failed");
+    let file = parse_file(tokens, id).expect("Parsing failed");
+    simplify_file(file).expect("AST Simplification failed")
+}
+
+fn parse_children_then_add(
+    files: &mut Vec<(simplified_ast::File, PathBuf)>,
+    tree: &mut FileTree,
+    file_id: FileId,
+) {
+    let (file, file_path) = files
+        .iter()
+        .find(|(file, _)| file.id == file_id)
+        .expect("Invalid file_id");
+    let mod_names = get_mod_names(file);
+    let file_path = file_path.clone();
+
+    for mod_name in &mod_names {
+        let child_file_id = get_unused_file_id(files);
+        tree.add_child(file_id, mod_name, child_file_id)
+            .expect("Multiple modules with same name.");
+
+        let (child_src, child_path) = {
+            if !file_path.ends_with("mod.ph") {
+                panic!("{:?} cannot have submodules.", file_path);
+            }
+            let child_leaf_file_path = file_path
+                .parent()
+                .unwrap()
+                .join(mod_name.src_str())
+                .with_extension("ph");
+            let child_nonleaf_file_path = file_path
+                .parent()
+                .unwrap()
+                .join(mod_name.src_str())
+                .join("mod.ph");
+
+            fs::read_to_string(&child_leaf_file_path)
+                .map(|src| (src, child_leaf_file_path))
+                .or_else(|_| {
+                    fs::read_to_string(&child_nonleaf_file_path)
+                        .map(|src| (src, child_nonleaf_file_path))
+                })
+                .expect("Failed to open file")
+        };
+        let child_file = lex_and_parse_file(&child_src, child_file_id);
+        files.push((child_file, child_path));
+
+        parse_children_then_add(files, tree, child_file_id);
+    }
+}
+
+fn get_unused_file_id(files: &[(simplified_ast::File, PathBuf)]) -> FileId {
+    let max_raw = files.iter().map(|(file, _)| file.id.0).max().unwrap_or(0);
+    FileId(max_raw + 1)
+}
+
+fn get_mod_names(file: &simplified_ast::File) -> Vec<IdentifierName> {
+    let mut mod_names = vec![];
+    for item in &file.items {
+        if let simplified_ast::FileItem::Mod(mod_) = item {
+            mod_names.push(mod_.name.name.clone());
+        }
+    }
+    mod_names
+}
+
 #[test]
 fn factorial() {
-    // TODO: Automatically build from directory.
-    let pack = FileBuilder {
-        id: FileId(0),
-        src: include_str!(
-            "../../../sample_code/should_succeed/multi_file/no_warnings/factorial/mod.ph"
-        ),
-    };
-    let nat = FileBuilder {
-        id: FileId(1),
-        src: include_str!(
-            "../../../sample_code/should_succeed/multi_file/no_warnings/factorial/nat.ph"
-        ),
-    };
-    let ops = FileBuilder {
-        id: FileId(2),
-        src: include_str!(
-            "../../../sample_code/should_succeed/multi_file/no_warnings/factorial/ops.ph"
-        ),
-    };
-    let unused = FileBuilder {
-        id: FileId(3),
-        src: include_str!(
-            "../../../sample_code/should_succeed/multi_file/no_warnings/factorial/unused.ph"
-        ),
-    };
-    expect_success_with_no_warnings(
-        FileTreeBuilder {
-            root: pack.id,
-            edges_to_children: vec![
-                (pack.id, "nat", nat.id),
-                (pack.id, "ops", ops.id),
-                (pack.id, "unused", unused.id),
-            ],
-        },
-        vec![pack, nat, ops, unused],
-    );
+    expect_success_with_no_warnings(checked_path!(
+        "../../../sample_code/should_succeed/multi_file/no_warnings/factorial/pack.omlet"
+    ));
 }
--- pamc/src/tests/sample_code/should_succeed/multi_file/no_warnings/factorial/unused.ph
+++
@@ -1,4 +0,0 @@
-//! This file should not be bound, so even though
-//! it's invalid, the build should still succeed.
-
-let x = this_name_does_not_exist;
