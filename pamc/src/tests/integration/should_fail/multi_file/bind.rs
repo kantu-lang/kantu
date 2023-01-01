e6b7116a79027af07fe047b8c531adb47549defe
Kyle Lin <kylejlin.us@gmail.com>
2022-12-31 23:27:36 -0800

    Extract ProjectPath

--- pamc/src/tests/integration/should_fail/multi_file/bind.rs
+++ pamc/src/tests/integration/should_fail/multi_file/bind.rs
@@ -1,22 +1,7 @@
 use super::*;
 
-/// `file_path` should **always** be `file!()`.
-///
-/// This is so it will be consistent with `checked_unadjusted_pack_omlet_path`.
-///
-/// The reason we make `file_path` a parameter rather than simply
-/// hardcoding `file!()` in the function is because the value
-/// of `file!()` will change depending on where it is written.
-/// The value of `checked_unadjusted_pack_omlet_path` is relative to the
-/// calling file, so the value of `file_path` must also be relative to the calling file.
-/// Thus, we cannot hardcode `file!()` in the function definition,
-/// and must instead require the caller to pass it in as an argument.
-fn expect_bind_error(
-    file_path: &str,
-    checked_unadjusted_pack_omlet_path: &str,
-    panicker: impl FnOnce(BindError),
-) {
-    let (files, file_tree) = get_files_and_file_tree(file_path, checked_unadjusted_pack_omlet_path);
+fn expect_bind_error(project_path: ProjectPath, panicker: impl FnOnce(BindError)) {
+    let (files, file_tree) = get_files_and_file_tree(project_path);
     let err = bind_files(file_tree.root(), files, &file_tree)
         .expect_err("Binding unexpectedly succeeded");
     panicker(err);
@@ -25,8 +10,12 @@ fn expect_bind_error(
 #[test]
 fn name_clash() {
     expect_bind_error(
-        file!(),
-        checked_path!("../../../sample_code/should_fail/multi_file/bind/name_clash/pack.omlet"),
+        ProjectPath {
+            callee_file_path: file!(),
+            checked_unadjusted_pack_omlet_path: checked_path!(
+                "../../../sample_code/should_fail/multi_file/bind/name_clash/pack.omlet"
+            ),
+        },
         |err| match err {
             BindError::NameClash(NameClashError {
                 name,
@@ -43,10 +32,12 @@ fn name_clash() {
 #[test]
 fn leaky_use_single() {
     expect_bind_error(
-        file!(),
-        checked_path!(
-            "../../../sample_code/should_fail/multi_file/bind/leaky_use_single/pack.omlet"
-        ),
+        ProjectPath {
+            callee_file_path: file!(),
+            checked_unadjusted_pack_omlet_path: checked_path!(
+                "../../../sample_code/should_fail/multi_file/bind/leaky_use_single/pack.omlet"
+            ),
+        },
         |err| match err {
             BindError::CannotLeakPrivateName(CannotLeakPrivateNameError {
                 name_component,
@@ -63,8 +54,12 @@ fn leaky_use_single() {
 #[test]
 fn leaky_use_single_nested() {
     expect_bind_error(
-        file!(),
-        checked_path!("../../../sample_code/should_fail/multi_file/bind/use_priv/pack.omlet"),
+        ProjectPath {
+            callee_file_path: file!(),
+            checked_unadjusted_pack_omlet_path: checked_path!(
+                "../../../sample_code/should_fail/multi_file/bind/use_priv/pack.omlet"
+            ),
+        },
         |err| match err {
             BindError::NameIsPrivate(NameIsPrivateError {
                 name_component,
@@ -81,8 +76,12 @@ fn leaky_use_single_nested() {
 #[test]
 fn leaky_type() {
     expect_bind_error(
-        file!(),
-        checked_path!("../../../sample_code/should_fail/multi_file/bind/leaky_type/pack.omlet"),
+        ProjectPath {
+            callee_file_path: file!(),
+            checked_unadjusted_pack_omlet_path: checked_path!(
+                "../../../sample_code/should_fail/multi_file/bind/leaky_type/pack.omlet"
+            ),
+        },
         |err| match err {
             BindError::CannotLeakPrivateName(CannotLeakPrivateNameError {
                 name_component,
