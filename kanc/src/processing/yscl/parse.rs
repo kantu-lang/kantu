c4b2a02d6ba7d68f441c46e05cb783f99d41db42
Kyle Lin <kylejlin.us@gmail.com>
2023-01-03 05:36:17 -0800

    SQUASH Parse atomic value eagerly

--- kanc/src/processing/yscl/parse.rs
+++ kanc/src/processing/yscl/parse.rs
@@ -6,7 +6,7 @@ mod unfinished {
 
     #[derive(Clone, Debug, PartialEq, Eq, Hash)]
     pub enum Unfinished {
-        AtomicSrc(String),
+        AtomicValue(String),
         List(UnfinishedList),
         Map(UnfinishedMap),
     }
@@ -61,13 +61,10 @@ pub fn parse(src: &str) -> Result<Map, usize> {
 
     while let Some((i, c)) = remaining.next() {
         match stack.last_mut().expect("Stack should never be empty") {
-            Unfinished::AtomicSrc(atomic_src) => match c {
+            Unfinished::AtomicValue(atomic_value) => match c {
                 '\n' => return Err(i),
                 '"' => {
-                    atomic_src.push(c);
-                    let top = Node::Atomic(AtomicValue(
-                        parse_atomic_value(atomic_src).expect("Atomic value should be well-formed"),
-                    ));
+                    let top = Node::Atomic(AtomicValue(atomic_value.clone()));
                     stack.pop().unwrap();
                     if let Some(return_val) =
                         reduce(&mut stack, top).expect(REDUCE_SHOULD_SUCCEED_MSG)
@@ -76,14 +73,13 @@ pub fn parse(src: &str) -> Result<Map, usize> {
                     }
                 }
                 '\\' => {
-                    atomic_src.push(c);
                     let Some((next_i, next_c)) = remaining.next() else {
                         return unexpected_eoi_err;
                     };
                     match next_c {
-                        '\\' | '"' | 'n' => atomic_src.push(next_c),
+                        '\\' | '"' | 'n' => atomic_value.push(next_c),
                         'u' => {
-                            atomic_src.push(next_c);
+                            let mut hex = String::with_capacity(4);
                             for _ in 0..4 {
                                 let Some((next_i, next_c)) = remaining.next() else {
                                     return unexpected_eoi_err;
@@ -91,13 +87,19 @@ pub fn parse(src: &str) -> Result<Map, usize> {
                                 if !next_c.is_ascii_hexdigit() {
                                     return Err(next_i);
                                 }
-                                atomic_src.push(next_c);
+                                hex.push(next_c);
                             }
+                            let codepoint = u32::from_str_radix(&hex, 16)
+                                .expect("Hex code should always be valid");
+                            let Some(encoded_char) = std::char::from_u32(codepoint) else {
+                                return Err(next_i + 4);
+                            };
+                            atomic_value.push(encoded_char);
                         }
                         _ => return Err(next_i),
                     }
                 }
-                _other_char => atomic_src.push(c),
+                _other_char => atomic_value.push(c),
             },
 
             Unfinished::List(UnfinishedList {
@@ -119,7 +121,7 @@ pub fn parse(src: &str) -> Result<Map, usize> {
                     *needs_newline_before_next_element = false;
                 }
                 '"' => {
-                    stack.push(Unfinished::AtomicSrc("\"".to_string()));
+                    stack.push(Unfinished::AtomicValue("".to_string()));
                 }
                 '{' => {
                     stack.push(Unfinished::Map(UnfinishedMap {
@@ -201,7 +203,7 @@ pub fn parse(src: &str) -> Result<Map, usize> {
                     if !pending_entry.has_equal {
                         return Err(i);
                     }
-                    stack.push(Unfinished::AtomicSrc("\"".to_string()));
+                    stack.push(Unfinished::AtomicValue("".to_string()));
                 }
                 '{' => {
                     if !pending_entry.has_equal {
@@ -311,7 +313,7 @@ fn reduce(stack: &mut Vec<Unfinished>, top: Node) -> Result<Option<Map>, ()> {
             Node::Map(top) => Ok(Some(top)),
             _ => Err(()),
         },
-        Some(Unfinished::AtomicSrc(_)) => Err(()),
+        Some(Unfinished::AtomicValue(_)) => Err(()),
         Some(Unfinished::List(UnfinishedList {
             elements,
             needs_newline_before_next_element,
@@ -339,65 +341,6 @@ fn reduce(stack: &mut Vec<Unfinished>, top: Node) -> Result<Option<Map>, ()> {
             }
         }
     }
-}
-
-/// Leading or trailing whitespace is forbidden.
-fn parse_atomic_value(src_including_quotes: &str) -> Result<String, usize> {
-    let unexpected_eoi_error = Err(src_including_quotes.len());
-    let mut out = "".to_string();
-    let mut remaining = src_including_quotes.chars().enumerate();
-
-    loop {
-        match remaining.next() {
-            Some((_, '"')) => break,
-            Some((_, c)) if c.is_whitespace() => {}
-            _ => return unexpected_eoi_error,
-        }
-    }
-
-    while let Some((i, c)) = remaining.next() {
-        match c {
-            '\n' => return Err(i),
-            '"' => {
-                if i != src_including_quotes.len() - 1 {
-                    return Err(i);
-                }
-                return Ok(out);
-            }
-            '\\' => {
-                let Some((next_i, next_c)) = remaining.next() else {
-                    return unexpected_eoi_error;
-                };
-                match next_c {
-                    '"' => out.push('"'),
-                    '\\' => out.push('\\'),
-                    'n' => out.push('\n'),
-                    'u' => {
-                        let mut hex = String::with_capacity(4);
-                        for _ in 0..4 {
-                            let Some((next_i, next_c)) = remaining.next() else {
-                                return unexpected_eoi_error;
-                            };
-                            if !next_c.is_ascii_hexdigit() {
-                                return Err(next_i);
-                            }
-                            hex.push(next_c);
-                        }
-                        let codepoint =
-                            u32::from_str_radix(&hex, 16).expect("Hex code should always be valid");
-                        let Some(encoded_char) = std::char::from_u32(codepoint) else {
-                            return Err(next_i + 4);
-                        };
-                        out.push(encoded_char);
-                    }
-                    _ => return Err(next_i),
-                }
-            }
-            _ => out.push(c),
-        }
-    }
-
-    unexpected_eoi_error
 }
 
 fn is_identifier_char(c: char) -> bool {
