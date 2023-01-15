be7c6fc2d4e1852866f315a17187da0cb2448e8a
Kyle Lin <kylejlin.us@gmail.com>
2023-01-15 13:51:58 -0800

    Remove debug printlns

--- kanc/src/processing/type_check/eval.rs
+++ kanc/src/processing/type_check/eval.rs
@@ -411,23 +411,6 @@ fn get_param_name_ids(
 }
 
 fn evaluate_well_typed_fun(state: &mut State, fun_id: NodeId<Fun>) -> NormalFormId {
-    use crate::processing::test_utils::{expand_lightened::*, format::*};
-    static mut DEBUG: usize = 0;
-    unsafe {
-        DEBUG += 1;
-        if DEBUG == 30000 {
-            panic!("Oh no!");
-        }
-    }
-
-    println!(
-        "EVALUATING FUN: {}",
-        format_expression_with_default_options(&expand_expression(
-            state.registry,
-            ExpressionId::Fun(fun_id)
-        ))
-    );
-
     let fun = state.registry.get(fun_id).clone();
     let normalized_param_list_id =
         normalize_params_as_much_as_possible_and_leave_in_context(state, fun.param_list_id)
