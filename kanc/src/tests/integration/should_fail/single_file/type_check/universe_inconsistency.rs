1e925a5bf90b51da52e7598696a19260ffece7ce
Kyle Lin <kylejlin.us@gmail.com>
2023-01-16 23:10:46 -0800

    Explain another `ascribe`

--- kanc/src/tests/integration/should_fail/single_file/type_check/universe_inconsistency.rs
+++ kanc/src/tests/integration/should_fail/single_file/type_check/universe_inconsistency.rs
@@ -19,5 +19,34 @@ fn currys_paradox() {
 #[test]
 fn russells_paradox() {
     let src = include_str!("../../../../sample_code/should_fail/single_file/type_check/universe_inconsistency/russells_paradox.k");
-    expect_universe_inconsistency_error(src);
+    // expect_universe_inconsistency_error(src);
+    expect_type_check_error(src, |registry, err| match err {
+        TypeCheckError::TypeMismatch {
+            expression_id,
+            expected_type_id,
+            actual_type_id,
+        } => {
+            use crate::processing::test_utils::{expand_lightened::*, format::*};
+            println!(
+                "\n\nEXPRESSION: {}",
+                format_expression_with_default_options(&expand_expression(registry, expression_id))
+            );
+            println!(
+                "\n\nEXPECTED_TYPE: {}",
+                format_expression_with_default_options(&expand_expression(
+                    registry,
+                    expected_type_id.raw()
+                ))
+            );
+            println!(
+                "\n\nACTUAL_TYPE: {}",
+                format_expression_with_default_options(&expand_expression(
+                    registry,
+                    actual_type_id.raw()
+                ))
+            );
+            panic!("\n\nUNIMPLEMENTED. The error is: {:#?}", err);
+        }
+        other => panic!("UNIMPLEMENTED. The error is: {:#?}", other),
+    });
 }
