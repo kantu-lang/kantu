04f189279f116e70ffd764be33999cb73acba060
Kyle Lin <kylejlin.us@gmail.com>
2023-02-26 19:29:38 -0800

    Update conversion utils

--- docs/conversion_utils.js
+++ docs/conversion_utils.js
@@ -2,12 +2,12 @@
 // Kantu types (e.g., Nat, List)
 // to their native analogues (e.g., number, Array).
 
-function toNum(nat) {
+function pnatToNum(nat) {
   if (nat.length === 1) {
     return 0;
   }
   if (nat.length === 2) {
-    return 1 + toNum(nat[1]);
+    return 1 + pnatToNum(nat[1]);
   }
   throw { badNat: nat };
 }
@@ -20,4 +20,50 @@ function toArr(list) {
     return [list[2]].concat(toArr(list[3]));
   }
   throw { badList: list };
+}
+
+function intToNum(int) {
+  if (int[0] === "neg") {
+    return -posToNum(int[1]);
+  }
+  if (int[0] === "nat") {
+    return natToNum(int[1]);
+  }
+  throw { badInt: int };
+}
+
+function natToNum(nat) {
+  if (nat[0] === "zero") {
+    return 0;
+  }
+  if (nat[0] === "pos") {
+    return posToNum(nat[1]);
+  }
+  throw { badNat: nat };
+}
+
+function posToNum(pos) {
+  return parseInt(posToBitString(pos), 2);
+}
+
+function posToBitString(pos) {
+  if (pos[0] === "one") {
+    return "1";
+  }
+  if (pos[0] === "extend") {
+    const left = posToBitString(pos[1]);
+    const right = String(bitToNum(pos[2]));
+    return left + right;
+  }
+  throw { badPos: pos };
+}
+
+function bitToNum(bit) {
+  if (bit[0] === "zero") {
+    return 0;
+  }
+  if (bit[0] === "one") {
+    return 1;
+  }
+  throw { badBit: bit };
 }
